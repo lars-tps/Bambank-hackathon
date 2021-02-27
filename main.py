7273508b5e2ad28fec887c8a3e2513fc141adf43
ketler222 <36614221+ketler222@users.noreply.github.com>
2021-02-27 21:31:13 +0700

    put int templates
    sadsada

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from flask import Flask, url_for, redirect,render_template
 app= Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template("Homepage.html")
+
+if __name__=='__main__':
+    app.run(debug=True)
