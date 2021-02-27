92ed802699096e186fcae93bfa82354108233d7d
ketler222 <36614221+ketler222@users.noreply.github.com>
2021-02-27 15:54:17 +0700

    distance calc
    made the distance calculation in main func

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from qrcode_generator import make_qr
+from ClosestHospital import calculate,calcDistance
 import csv
 from priority_function import priority_sort
-if __name__=='__main__':
+def insert_profiles():
+    #getting the patient profiles and sorting them based on their priority level
     profiles=[]
     with open('data.csv','r') as csv_file:
         csv_reader=csv.reader(csv_file)
@@ -11,6 +13,19 @@ if __name__=='__main__':
                 residency_status=0 if line[5]=='Foreigner' else 1
                 profiles.append([line[0],int(line[1]),line[2],(line[3],line[4]),residency_status])
             start=True
-    temp=priority_sort(profiles)
+    profiles=priority_sort(profiles)
+    return profiles
+
+def insert_to_hospital(profiles):
+    with open('hospital_data.csv','r') as csv_file:
+        hospital=csv.reader(csv_file)
+        for line in hospital: 
+            for i in profiles:
+                print(calcDistance(int(i[3][0]),int(i[3][1]),int(line[3]),int(line[4])))
+
+if __name__=='__main__':
+    profiles=insert_profiles()
+    insert_to_hospital(profiles)
+
     
     
