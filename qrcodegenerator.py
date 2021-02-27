c2eb02affa60f42a43738c7bbd014206608e8cde
ketler222 <36614221+ketler222@users.noreply.github.com>
2021-02-27 12:02:16 +0700

    added parameter to qrcode func
    added another parameter which is the web associated to the hospital which will  be giving the vaccine to the patient

--- qrcodegenerator.py
+++ qrcodegenerator.py
@@ -1,12 +1,12 @@
 import qrcode
-def make_qr(file_name):
+def make_qr(hospital_web,file_name):
     qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=7,
     border=4,
     )
-    qr.add_data('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    qr.add_data(hospital_web)
     qr.make(fit=True)
     img=qr.make_image(fill='black',back_color='white')
     img.save('qrcode/'+file_name+'.png')
