fd54ddf76c186eb4e28d501f592bc9307ebc7c65
ketler222 <36614221+ketler222@users.noreply.github.com>
2021-02-27 22:04:31 +0700

    sadsad
    sadsadsa

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ app= Flask(__name__)
 def home():
     return render_template("Homepage.html")
 
+@app.route('/hospital_page',methods=["POST","GET"])
+def hospital_page():
+    if request.method=="POST":
+        user=request.form["hosp"]
+    else:
+        return render_template('hospital.html')
+
 if __name__=='__main__':
     app.run(debug=True)
